00a497715d9cdcbc65e75e24f0cee65ca24a091d
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2025-03-10 13:03:43 +0600

    Update 2894.java
    add update solutions

--- 2894.java
+++ 2894.java
@@ -1,3 +1,13 @@
+// 1st
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int x = n / m;
+        int num2 = m * x * ( x + 1) / 2;
+        int num1 = n * (n + 1) / 2 - num2;
+        return num1 - num2;
+    }
+}
+// 2nd 
 class Solution {
     public int differenceOfSums(int n, int m) {
     /// optional
