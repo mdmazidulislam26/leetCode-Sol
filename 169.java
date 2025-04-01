c940353ec12eee8991d5edd0e27e1e776bb20a72
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2025-04-01 20:21:45 +0600

    Update 169.java

--- 169.java
+++ 169.java
@@ -2,7 +2,7 @@ class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         int majorElement=nums[0];
-        int prvCount=0;
+        int prvCount=00;
         int count=1;
         for(int i =0; i < nums.length-1; i++){
             if(nums[i] == nums[i+1]){
